1cf723f69b0162828760fbd98066b0dff538acbe
Agustin Berge <agustinberge@gmail.com>
2025-03-06 12:36:14 -0300

    On basic-impl: reverse

--- thrust/thrust/system/hpx/detail/reverse.h
+++ thrust/thrust/system/hpx/detail/reverse.h
@@ -14,6 +14,10 @@
  *  limitations under the License.
  */
 
+/*! \file reverse.h
+ *  \brief HPX implementation of reverse/reverse_copy.
+ */
+
 #pragma once
 
 #include <thrust/detail/config.h>
@@ -25,6 +29,32 @@
 #elif defined(_CCCL_IMPLICIT_SYSTEM_HEADER_MSVC)
 #  pragma system_header
 #endif // no system header
+#include <thrust/system/hpx/detail/execution_policy.h>
 
-// this system inherits reverse
-#include <thrust/system/cpp/detail/reverse.h>
+#include <hpx/parallel/algorithms/reverse.hpp>
+
+THRUST_NAMESPACE_BEGIN
+namespace system
+{
+namespace hpx
+{
+namespace detail
+{
+
+template <typename DerivedPolicy, typename BidirectionalIterator>
+void reverse(execution_policy<DerivedPolicy>&, BidirectionalIterator first, BidirectionalIterator last)
+{
+  return ::hpx::reverse(first, last);
+}
+
+template <typename DerivedPolicy, typename BidirectionalIterator, typename OutputIterator>
+OutputIterator reverse_copy(
+  execution_policy<DerivedPolicy>&, BidirectionalIterator first, BidirectionalIterator last, OutputIterator result)
+{
+  return ::hpx::reverse_copy(first, last, result);
+}
+
+} // end namespace detail
+} // end namespace hpx
+} // end namespace system
+THRUST_NAMESPACE_END
