8f8937ff420f5dedc8e21f1c3fe690c606007735
Agustin Berge <agustinberge@gmail.com>
2025-03-06 11:49:11 -0300

    Impl remove/remove_copy/remove_if/remove_copy_if

--- thrust/thrust/system/hpx/detail/remove.h
+++ thrust/thrust/system/hpx/detail/remove.h
@@ -14,6 +14,10 @@
  *  limitations under the License.
  */
 
+/*! \file remove.h
+ *  \brief HPX implementation of remove/remove_copy/remove_if/remove_copy_if.
+ */
+
 #pragma once
 
 #include <thrust/detail/config.h>
@@ -25,6 +29,45 @@
 #elif defined(_CCCL_IMPLICIT_SYSTEM_HEADER_MSVC)
 #  pragma system_header
 #endif // no system header
+#include <thrust/system/hpx/detail/execution_policy.h>
 
-// this system inherits remove
-#include <thrust/system/cpp/detail/remove.h>
+#include <hpx/parallel/algorithms/remove.hpp>
+#include <hpx/parallel/algorithms/remove_copy.hpp>
+
+THRUST_NAMESPACE_BEGIN
+namespace system
+{
+namespace hpx
+{
+namespace detail
+{
+template <typename DerivedPolicy, typename ForwardIterator, typename T>
+ForwardIterator remove(execution_policy<DerivedPolicy>&, ForwardIterator first, ForwardIterator last, const T& value)
+{
+  return ::hpx::remove(first, last, value);
+}
+
+template <typename DerivedPolicy, typename InputIterator, typename OutputIterator, typename T>
+OutputIterator remove_copy(
+  execution_policy<DerivedPolicy>&, InputIterator first, InputIterator last, OutputIterator result, const T& value)
+{
+  return ::hpx::remove_copy(first, last, result, value);
+}
+
+template <typename DerivedPolicy, typename ForwardIterator, typename Predicate>
+ForwardIterator remove_if(execution_policy<DerivedPolicy>&, ForwardIterator first, ForwardIterator last, Predicate pred)
+{
+  return ::hpx::remove_if(first, last, pred);
+}
+
+template <typename DerivedPolicy, typename InputIterator, typename OutputIterator, typename Predicate>
+OutputIterator remove_copy_if(
+  execution_policy<DerivedPolicy>&, InputIterator first, InputIterator last, OutputIterator result, Predicate pred)
+{
+  return ::hpx::remove_copy_if(first, last, result, pred);
+}
+
+} // end namespace detail
+} // end namespace hpx
+} // end namespace system
+THRUST_NAMESPACE_END
