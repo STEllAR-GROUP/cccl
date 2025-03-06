f5b7508b26392774b853d16a2e1378de26d9d6f0
Agustin Berge <agustinberge@gmail.com>
2025-03-06 11:58:05 -0300

    Impl replace/replace_copy/replace_if/replace_copy_if

--- thrust/thrust/system/hpx/detail/replace.h
+++ thrust/thrust/system/hpx/detail/replace.h
@@ -14,6 +14,10 @@
  *  limitations under the License.
  */
 
+/*! \file replace.h
+ *  \brief HPX implementation of replace/replace_copy/replace_if/replace_copy_if.
+ */
+
 #pragma once
 
 #include <thrust/detail/config.h>
@@ -25,6 +29,57 @@
 #elif defined(_CCCL_IMPLICIT_SYSTEM_HEADER_MSVC)
 #  pragma system_header
 #endif // no system header
+#include <thrust/system/hpx/detail/execution_policy.h>
 
-// this system inherits replace
-#include <thrust/system/cpp/detail/replace.h>
+#include <hpx/parallel/algorithms/replace.hpp>
+
+THRUST_NAMESPACE_BEGIN
+namespace system
+{
+namespace hpx
+{
+namespace detail
+{
+
+template <typename DerivedPolicy, typename ForwardIterator, typename T>
+void replace(
+  execution_policy<DerivedPolicy>&, ForwardIterator first, ForwardIterator last, const T& old_value, const T& new_value)
+{
+  return ::hpx::replace(first, last, old_value, new_value);
+}
+
+template <typename DerivedPolicy, typename ForwardIterator, typename Predicate, typename T>
+void replace_if(
+  execution_policy<DerivedPolicy>& exec, ForwardIterator first, ForwardIterator last, Predicate pred, const T& new_value)
+{
+  return ::hpx::replace_if(first, last, pred, new_value);
+}
+
+template <typename DerivedPolicy, typename InputIterator, typename OutputIterator, typename T>
+OutputIterator replace_copy(
+  execution_policy<DerivedPolicy>&,
+  InputIterator first,
+  InputIterator last,
+  OutputIterator result,
+  const T& old_value,
+  const T& new_value)
+{
+  return ::hpx::replace_copy(first, last, result, old_value, new_value);
+}
+
+template <typename DerivedPolicy, typename InputIterator, typename OutputIterator, typename Predicate, typename T>
+OutputIterator replace_copy_if(
+  execution_policy<DerivedPolicy>&,
+  InputIterator first,
+  InputIterator last,
+  OutputIterator result,
+  Predicate pred,
+  const T& new_value)
+{
+  return ::hpx::replace_copy_if(first, last, result, pred, new_value);
+}
+
+} // end namespace detail
+} // end namespace hpx
+} // end namespace system
+THRUST_NAMESPACE_END
