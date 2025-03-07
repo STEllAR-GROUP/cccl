d3257924428978cc5c767de18dc7ad55e62a16b3
Agustin Berge <agustinberge@gmail.com>
2025-03-07 10:21:31 -0300

    On basic-impl: uninitialized_fill

--- thrust/thrust/system/hpx/detail/uninitialized_fill.h
+++ thrust/thrust/system/hpx/detail/uninitialized_fill.h
@@ -1,9 +1,9 @@
 /*
- *  Copyright 2008-2025 NVIDIA Corporation
+ *  fillright 2008-2025 NVIDIA Corporation
  *
  *  Licensed under the Apache License, Version 2.0 (the "License");
  *  you may not use this file except in compliance with the License.
- *  You may obtain a copy of the License at
+ *  You may obtain a fill of the License at
  *
  *      http://www.apache.org/licenses/LICENSE-2.0
  *
@@ -12,6 +12,10 @@
  *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  *  See the License for the specific language governing permissions and
  *  limitations under the License.
+ */
+
+/*! \file uninitialized_fill.h
+ *  \brief HPX implementation of uninitialized_fill/uninitialized_fill_n.
  */
 
 #pragma once
@@ -25,6 +29,31 @@
 #elif defined(_CCCL_IMPLICIT_SYSTEM_HEADER_MSVC)
 #  pragma system_header
 #endif // no system header
+#include <thrust/system/hpx/detail/execution_policy.h>
 
-// this system inherits uninitialized_fill
-#include <thrust/system/cpp/detail/uninitialized_fill.h>
+#include <hpx/parallel/algorithms/uninitialized_fill.hpp>
+
+THRUST_NAMESPACE_BEGIN
+namespace system
+{
+namespace hpx
+{
+namespace detail
+{
+
+template <typename DerivedPolicy, typename ForwardIterator, typename T>
+void uninitialized_fill(execution_policy<DerivedPolicy>&, ForwardIterator first, ForwardIterator last, const T& x)
+{
+  return ::hpx::uninitialized_fill(first, last, x);
+}
+
+template <typename DerivedPolicy, typename ForwardIterator, typename Size, typename T>
+ForwardIterator uninitialized_fill_n(execution_policy<DerivedPolicy>&, ForwardIterator first, Size n, const T& x)
+{
+  return ::hpx::uninitialized_fill_n(first, n, x);
+}
+
+} // end namespace detail
+} // end namespace hpx
+} // end namespace system
+THRUST_NAMESPACE_END
