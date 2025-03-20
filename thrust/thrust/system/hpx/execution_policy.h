0837d5f130263df1abc5d35c9e03cbd4965dc004
Agustin Berge <agustinberge@gmail.com>
2025-03-20 17:10:17 -0300

    Comment on hpx_main usage

--- thrust/thrust/system/hpx/execution_policy.h
+++ thrust/thrust/system/hpx/execution_policy.h
@@ -30,6 +30,7 @@
 #  pragma system_header
 #endif // no system header
 
+// FIXME(K-ballo): this macro hack is used to automatically start the runtime in tests and examples
 #include <hpx/hpx_main.hpp>
 
 // get the execution policies definitions first
