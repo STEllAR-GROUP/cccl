1efbd2a59c50d4870efddaefa011fa50dcab6773
Agustin Berge <agustinberge@gmail.com>
2025-04-13 17:04:01 -0300

    Add remaining execution policies

--- thrust/testing/hpx/reduce.cu
+++ thrust/testing/hpx/reduce.cu
@@ -23,13 +23,13 @@ void TestReducePar()
 }
 DECLARE_UNITTEST(TestReducePar);
 
-void TestReduceParOnParallelExecutor()
+void TestReduceParOnSequencedExecutor()
 {
-  hpx::execution::parallel_executor exec{};
+  hpx::execution::sequenced_executor exec{};
 
   TestReduce(thrust::hpx::par.on(exec));
 }
-DECLARE_UNITTEST(TestReduceParOnParallelExecutor);
+DECLARE_UNITTEST(TestReduceParOnSequencedExecutor);
 
 void TestReduceParOnForkJoinExecutor()
 {
@@ -46,3 +46,93 @@ void TestReduceParWithAutoChunkSize()
   TestReduce(thrust::hpx::par.with(acs));
 }
 DECLARE_UNITTEST(TestReduceParWithAutoChunkSize);
+
+void TestReduceParUnseq()
+{
+  TestReduce(thrust::hpx::par_unseq);
+}
+DECLARE_UNITTEST(TestReduceParUnseq);
+
+void TestReduceParUnseqOnSequencedExecutor()
+{
+  hpx::execution::sequenced_executor exec{};
+
+  TestReduce(thrust::hpx::par_unseq.on(exec));
+}
+DECLARE_UNITTEST(TestReduceParUnseqOnSequencedExecutor);
+
+void TestReduceParUnseqOnForkJoinExecutor()
+{
+  hpx::execution::experimental::fork_join_executor exec{};
+
+  TestReduce(thrust::hpx::par_unseq.on(exec));
+}
+DECLARE_UNITTEST(TestReduceParUnseqOnForkJoinExecutor);
+
+void TestReduceParUnseqWithAutoChunkSize()
+{
+  hpx::execution::experimental::auto_chunk_size acs{};
+
+  TestReduce(thrust::hpx::par_unseq.with(acs));
+}
+DECLARE_UNITTEST(TestReduceParUnseqWithAutoChunkSize);
+
+void TestReduceSeq()
+{
+  TestReduce(thrust::hpx::seq);
+}
+DECLARE_UNITTEST(TestReduceSeq);
+
+void TestReduceSeqOnSequencedExecutor()
+{
+  hpx::execution::sequenced_executor exec{};
+
+  TestReduce(thrust::hpx::seq.on(exec));
+}
+DECLARE_UNITTEST(TestReduceSeqOnSequencedExecutor);
+
+//void TestReduceSeqOnForkJoinExecutor()
+//{
+//  hpx::execution::experimental::fork_join_executor exec{};
+//
+//  TestReduce(thrust::hpx::seq.on(exec));
+//}
+//DECLARE_UNITTEST(TestReduceSeqOnForkJoinExecutor);
+
+void TestReduceSeqWithAutoChunkSize()
+{
+  hpx::execution::experimental::auto_chunk_size acs{};
+
+  TestReduce(thrust::hpx::seq.with(acs));
+}
+DECLARE_UNITTEST(TestReduceSeqWithAutoChunkSize);
+
+void TestReduceUnseq()
+{
+  TestReduce(thrust::hpx::unseq);
+}
+DECLARE_UNITTEST(TestReduceUnseq);
+
+void TestReduceUnseqOnSequencedExecutor()
+{
+  hpx::execution::sequenced_executor exec{};
+
+  TestReduce(thrust::hpx::unseq.on(exec));
+}
+DECLARE_UNITTEST(TestReduceUnseqOnSequencedExecutor);
+
+//void TestReduceUnseqOnForkJoinExecutor()
+//{
+//  hpx::execution::experimental::fork_join_executor exec{};
+//
+//  TestReduce(thrust::hpx::unseq.on(exec));
+//}
+//DECLARE_UNITTEST(TestReduceUnseqOnForkJoinExecutor);
+
+void TestReduceUnseqWithAutoChunkSize()
+{
+  hpx::execution::experimental::auto_chunk_size acs{};
+
+  TestReduce(thrust::hpx::unseq.with(acs));
+}
+DECLARE_UNITTEST(TestReduceUnseqWithAutoChunkSize);
